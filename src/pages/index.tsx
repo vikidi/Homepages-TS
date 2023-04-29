840bd81deb9057c5d85b809fd370414858e653ef
Ville Saarinen <ville.saarinen9@gmail.com>
2023-04-30 02:27:11 +0300

    Setup typing animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function Home() {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "This is my <b>HOMEPAGE</b>",
+        "This is my <b>PORTFOLIO</b>",
+        "This is my <b>RANDOM PROJECT</b>",
+        "This is my <b>LEARNING OPPORTUNITY</b>",
+        "This is my <b>SHOW OFF</b>",
+        "This is my <b>THEN, NOW</b> and <b>FUTURE</b>",
+        "This is my <b>JOURNEY</b>",
+        "This is my <b>LIFE</b>^2000 <i>and it's now or never</i>",
+      ],
+      typeSpeed: 100,
+      backSpeed: 50,
+      smartBackspace: true,
+      loop: true,
+      backDelay: 1000,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div>
-      <h1>HOME</h1>
+      <h1>
+        <span style={{ whiteSpace: "pre" }} ref={el} />
+      </h1>
     </div>
   );
 }
