ba431965ce1ed6fe2a358fc03b5484266c374ecf
Ville Saarinen <ville.saarinen9@gmail.com>
2023-05-03 20:39:06 +0300

    rename part2

---
+++ src/components/Navbar/desktopNavbar.tsx
@@ -0,0 +1,34 @@
+import { NavLink, NavLinks } from "@/types";
+import Link from "next/link";
+import { Logo } from "../logo";
+
+export default function DesktopNavbar({ links }: { links: NavLinks }) {
+  return (
+    <div className="flex w-full bg-slate-800 shadow-md backdrop-blur-md max-md:hidden">
+      <div className="absolute bottom-0 left-0 right-0 top-0 ml-5 flex items-center text-zinc-300 lg:ml-10">
+        <Logo padding="p-0" border={false} />
+      </div>
+      <ul className="z-10 my-[1px] flex w-full flex-row justify-center">
+        {links.map((link: NavLink, i: number) => (
+          <li
+            key={link.text}
+            className="relative border-b-2 border-green-300 border-opacity-0 p-4 text-zinc-300 hover:border-opacity-100 hover:text-zinc-400"
+          >
+            {i === 0 ? (
+              <Link className="m-2 lg:mx-10" href={link.path}>
+                {link.text}
+              </Link>
+            ) : (
+              <Link
+                className="after:content[''] m-2 after:absolute after:bottom-[27%] after:left-0 after:h-2/5 after:w-[1px] after:bg-white/30 lg:mx-6"
+                href={link.path}
+              >
+                {link.text}
+              </Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Navbar/mobileNavbar.tsx
@@ -0,0 +1,77 @@
+import { NavLink, NavLinks } from "@/types";
+import Link from "next/link";
+import { Logo } from "../logo";
+
+export default function MobileNavbar({
+  links,
+  navOpen,
+  setNavOpen,
+}: {
+  links: NavLinks;
+  navOpen: boolean;
+  setNavOpen: Function;
+}) {
+  const genericHamburgerLine =
+    "h-1 w-6 rounded-full bg-zinc-300 transition ease transform duration-300";
+
+  return (
+    <div className="flex h-full w-full flex-col md:hidden">
+      <div className="flex justify-end bg-slate-800 shadow-md backdrop-blur-md">
+        <div className="absolute bottom-0 left-0 right-0 top-0 ml-8 flex items-center text-zinc-300">
+          <Logo padding="p-0" border={false} />
+        </div>
+        <button
+          className="z-10 flex flex-col items-center justify-center px-10 py-4"
+          onClick={() => setNavOpen(!navOpen)}
+        >
+          <div
+            className={`${genericHamburgerLine} my-1 opacity-80 ${
+              navOpen ? "translate-y-2 rotate-45" : ""
+            }`}
+          />
+          <div
+            className={`${genericHamburgerLine} ${
+              navOpen ? "opacity-0" : "opacity-80"
+            }`}
+          />
+          <div
+            className={`${genericHamburgerLine} my-1 opacity-80 ${
+              navOpen ? "-translate-y-2 -rotate-45" : ""
+            }`}
+          />
+        </button>
+      </div>
+
+      <div
+        style={{
+          transition: "all 0.25s ease-in-out",
+          width: !navOpen ? "0" : "100%",
+          height: !navOpen ? "0" : "100%",
+          alignSelf: "self-end",
+        }}
+        className="flex transform justify-end transition"
+      >
+        <div
+          className="w-1/5 backdrop-blur-[2px] sm:w-2/5"
+          onClick={() => setNavOpen(false)}
+        ></div>
+        <ul className="flex w-4/5 flex-col justify-start bg-slate-800/80 pt-2 shadow-md backdrop-blur-md sm:w-3/5">
+          {links.map((link: NavLink) => (
+            <li
+              key={link.text}
+              className="ml-4 flex w-full justify-start border-t-[1px] border-green-300/30 p-4 text-zinc-300 last:border-b-[1px] hover:text-zinc-400"
+            >
+              <Link
+                href={link.path}
+                onClick={() => setNavOpen(false)}
+                className="w-full"
+              >
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
